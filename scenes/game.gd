514600eb11651e87161b1a55066e847b0aec14b5
Rafael  Viol <126819742+RafaZenitus@users.noreply.github.com>
2025-03-03 19:26:19 -0300

    gameCompleted

--- scenes/game.gd
+++ scenes/game.gd
@@ -1,10 +1,12 @@
 extends Node2D
 
+const EXPLODE = preload("res://assets/explode.wav") #preload & load 
+
 @export var gem_scene: PackedScene
 
 @onready var label: Label = $Label #arrastar Label + cntrl
 @onready var timer: Timer = $Timer
-
+@onready var audio_stream_player_2d: AudioStreamPlayer2D = $AudioStreamPlayer2D
 
 var _score: int = 0
 
@@ -29,8 +31,14 @@ func stop_all() -> void:
 	for child in get_children():
 		child.set_process(false)
 
+func play_dead() -> void:
+	audio_stream_player_2d.stop()
+	audio_stream_player_2d.stream = EXPLODE
+	audio_stream_player_2d.play()
+
 func game_over() -> void:
 	stop_all()
+	play_dead()
 
 func _on_timer_timeout() -> void:
 	spawn_gem()
@@ -40,4 +48,6 @@ func _on_paddle_area_entered(area: Area2D) -> void:
 	_score += 1
 	#label.text = str(_score)
 	label.text = "%05d" % _score
+	audio_stream_player_2d.position = area.position
+	audio_stream_player_2d.play()
 	area.queue_free()
